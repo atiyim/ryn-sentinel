09d8144f42d698e550edb151554e1e3376939f82
atiyim <respecteddd3@gmail.com>
2026-05-28 21:22:40 +0300

    initial commit

---
+++ preload.js
@@ -0,0 +1,9 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  logToSheets: (data) => ipcRenderer.invoke('log-to-sheets', data),
+  onClipboardProof: (cb) => ipcRenderer.on('clipboard-proof', (_e, url) => cb(url)),
+  onClipboardCmd:   (cb) => ipcRenderer.on('clipboard-cmd',   (_e, cmd) => cb(cmd)),
+  copyText: (text) => ipcRenderer.invoke('copy-text', text),
+  openDiscord: (url) => ipcRenderer.invoke('open-discord', url),
+});
